f87ec874e89117f448a9c26b75e237e3546bc1d4
Sukunnari <151819685+sukunnari@users.noreply.github.com>
2026-07-26 20:05:38 +0800

    Remove puppeteer - It was actually never feasible to crawl the lb with a browser

--- src/tools/crawl-daily-update.ts
+++ src/tools/crawl-daily-update.ts
@@ -1,4 +1,3 @@
-import puppeteer from "puppeteer";
 import { db } from "../database/db.js";
 import { players, daily_tracker } from "../database/schema.js";
 import { eq, sql } from "drizzle-orm";
@@ -6,117 +5,6 @@ import { ConsolePrefixed } from "../utils/console-prefixed.js";
 const consolePref = new ConsolePrefixed("[crawlAndUpdateDailyPlayers]");
 import { parse as htmlParse } from "node-html-parser";
 import { sleep } from "../utils/sleep.js";
-
-async function getPlayersFromLeaderboardCrawlWithBrowser() {
-	const BASE_URL = "https://osu.ppy.sh";
-	const TODAY = new Date();
-	const DATE_URL = `${TODAY.getUTCFullYear()}-${
-		TODAY.getUTCMonth() + 1
-	}-${TODAY.getUTCDate()}`;
-	const ENTRY_URL = `${BASE_URL}/rankings/daily-challenge/${DATE_URL}`;
-
-	// Setup Puppeteer browser instance
-	const browser = await puppeteer.launch({
-		browser: "firefox",
-		headless: true,
-	});
-	const page = await browser.newPage();
-	await page.setViewport({ width: 1080, height: 1024 });
-	page.setDefaultNavigationTimeout(90_000);
-
-	const allDailyPlayers: { id: number; name: string }[] = [];
-
-	let browsingUrl: string | null = ENTRY_URL;
-	while (browsingUrl) {
-		const currentBrowsingUrl: string = browsingUrl;
-		let nextBrowsingUrl = null;
-		consolePref.info(`Navigating ${browsingUrl}`);
-		try {
-			// Navigate to the specified url
-			await page.goto(currentBrowsingUrl);
-			// Check out interactivity before doing anything
-			await page.locator(".ranking-page-table > tbody").hover();
-
-			const playerListContainer = await page
-				.locator(".ranking-page-table > tbody")
-				.waitHandle();
-
-			const playersPerPage: typeof allDailyPlayers =
-				await playerListContainer.evaluate((elem) => {
-					// Get data from within browser context (players)
-					// then pass it onto playersPerPage
-					const playerRows = elem.querySelectorAll("tr");
-					const players: typeof allDailyPlayers = [];
-
-					playerRows.forEach((row) => {
-						// Try to get player id & name
-						const retrievedId = parseInt(
-							row
-								.querySelector(".ranking-page-table-main__link.js-usercard")
-								?.getAttribute("data-user-id") ?? "",
-						);
-						const retrievedName = row.querySelector(
-							".ranking-page-table-main__link.js-usercard > span",
-						)?.textContent;
-
-						// Push values with their correct types
-						players.push({
-							id: isNaN(retrievedId) ? 0 : retrievedId,
-							name: retrievedName ?? "",
-						});
-					});
-					// Pass retrieved values
-					return players;
-				});
-
-			playersPerPage.forEach((player) => {
-				if (player.id || player.name) {
-					allDailyPlayers.push({
-						id: player.id,
-						name: player.name.trim(),
-					});
-				}
-			});
-
-			const navContainer = await page.locator("nav.pagination-v2").waitHandle();
-
-			const detectedNextPageurl: string | null = await navContainer.evaluate(
-				(elem) => {
-					const nextPageLink = elem.querySelector("div:last-child > a");
-					if (nextPageLink?.tagName == "A") {
-						return nextPageLink.getAttribute("href") ?? null;
-					} else {
-						return null;
-					}
-				},
-			);
-
-			if (typeof detectedNextPageurl == "string") {
-				nextBrowsingUrl = detectedNextPageurl;
-			}
-		} catch (error) {
-			consolePref.error(error);
-			nextBrowsingUrl = null;
-		} finally {
-			if (typeof nextBrowsingUrl == "string") {
-				if (currentBrowsingUrl == nextBrowsingUrl) {
-					consolePref.error("Looping detected");
-					browsingUrl = null;
-				} else {
-					browsingUrl = nextBrowsingUrl;
-				}
-			} else {
-				consolePref.info("End of navigation");
-				browsingUrl = null;
-			}
-		}
-	}
-
-	consolePref.info("Closing browser session");
-	await browser.close();
-
-	return allDailyPlayers;
-}
 
 async function getPlayersFromLeaderboardCrawlWithParser() {
 	const BASE_URL = "https://osu.ppy.sh";
@@ -245,6 +133,5 @@ async function updatePlayersFromLeaderboardCrawl() {
 
 export {
 	updatePlayersFromLeaderboardCrawl,
-	getPlayersFromLeaderboardCrawlWithBrowser,
 	getPlayersFromLeaderboardCrawlWithParser,
 };
